51f41f92b10070860a4a2e5b6024013f9fda95e6
Rajesh Vannala <rvannala@innominds.com>
2020-09-15 23:33:13 +0530

    login email and password validation

--- NodeJS/NodeMongoDB/src/models/usermodel.js
+++ NodeJS/NodeMongoDB/src/models/usermodel.js
@@ -1,7 +1,7 @@
 const mongoose = require("./../db/mongoose")
 const validator = require("validator");
-
-let User=mongoose.model('User',{
+const bcrypt = require("bcryptjs")
+let userSchema=new mongoose.Schema({
     name:{
         type:String
     },
@@ -10,7 +10,43 @@ let User=mongoose.model('User',{
     },
     reading:{
         type:Boolean
+    },
+    password:{
+        type:String
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+        lowercase:true,
+        validate(value){
+            if(!validator.isEmail(value))
+                throw new Error("invalid email");
+        }
+
     }
-});
+})
+
+userSchema.statics.findByCredentials = async (email , password) => {
+     const user = await User.findOne({email});
+     if(!user){
+        throw new Error("unable to login");
+     }
+     let isMatch = await bcrypt.compare(password , user.password);
+     if(!isMatch)
+        throw new Error("unable to login");
+     return user;
+}
+
+
+userSchema.pre('save' ,async function(next){
+    let user= this;
+    if(user.isModified('password')){
+        console.log(user['password'],"user['password']")
+        user['password']=await bcrypt.hash(user['password'],8)
+    }
+    next()
+})
+let User=mongoose.model('User', userSchema );
 
 module.exports=User;
--- NodeJS/NodeMongoDB/src/routes/user.js
+++ NodeJS/NodeMongoDB/src/routes/user.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = new express.Router();
 const User = require('./../models/usermodel');
+// const bcrypt = require("bcryptjs");
+// const { update } = require("./../models/usermodel");
+
+
+router.post('/user/login', async (req,res)=>{
+    try{
+        const users = await User.findByCredentials(req.body.email , req.body.password)
+        if(users)
+            res.status(200).send(users);
+    }
+    catch(err){
+        console.log(err)
+        res.status(400).send(err)
+    }
+})
 
 router.get("/userlist",async (req,res)=>{
     try{
-        let users = await User.find({});  
+        let users = await User.find({});
         res.status(200).send(users)
     }catch(e){
         res.status(400).send(e)
@@ -23,6 +38,7 @@ router.get('/getuser', async (req,res)=>{
 })
 
 router.post("/insertuser", async (req,res)=>{
+    console.log('im in insertuser')
     let user = new User(req.body)
     try{
         let users =await user.save();
@@ -52,13 +68,18 @@ router.patch( '/updateuser/:id' , async ( req , res )=>{
     try{
         let id = req.params.id;
         let body = req.body;
-        let allowupdates=["name","age"]
+        let allowupdates=["name","age","password"]
         let forupdates = Object.keys(body)
         let valid = allowupdates.every(allowupdate=>forupdates.includes(allowupdate));
         if( !valid ){
             return res.status(400).send("not allowed")
         }
-        let user= await User.findByIdAndUpdate( id , body , { new : true } );
+        let user= await User.findByIdAndUpdate( id );
+        forupdates.forEach(update=>{
+            console.log(body[update],update)
+            user[update]=body[update]
+        })
+        await user.save();
         res.status(200).send(user)
 
     }catch( e ) {
